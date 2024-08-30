eb201af5b9c83e45cb930a69b4bb01b673d0c0f1
Fenet-damena <fenetdamena74@gmail.com>
2024-08-30 04:14:16 -1200

    resume added

--- script.js
+++ script.js
@@ -7,12 +7,14 @@ const resetBtn = document.getElementById('reset');
 const lapBtn = document.getElementById('lap');
 const lapsList = document.getElementById('laps');
 let lapsCounter = 1;
+
 function displayTime() {
     const minutes = Math.floor(time / 60);
     const seconds = Math.floor(time % 60);
     const milliseconds = Math.floor((time % 1) * 100).toFixed(0);
     display.textContent = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}:${String(milliseconds).padStart(2, '0')}`;
 }
+
 function startTimer() {
     if (!isRunning) {
         timer = setInterval(() => {
@@ -27,11 +29,12 @@ function startTimer() {
         pauseTimer(); 
     }
 }
+
 function pauseTimer() {
     clearInterval(timer);
     isRunning = false;
-    startBtn.textContent = 'Start'; 
-    startBtn.style.backgroundColor = '';
+    startBtn.textContent = 'Resume'; 
+    startBtn.style.backgroundColor = ''; 
     lapBtn.disabled = true;
 }
 
@@ -42,10 +45,11 @@ function resetTimer() {
     lapsList.innerHTML = '';
     lapsCounter = 1;
     isRunning = false;
-    startBtn.textContent = 'Start';
+    startBtn.textContent = 'Start'; 
     startBtn.style.backgroundColor = ''; 
     lapBtn.disabled = true; 
 }
+
 function lapTimer() {
     const lapTime = display.textContent;
     const lapListItem = document.createElement('li');
@@ -53,6 +57,7 @@ function lapTimer() {
     lapsList.appendChild(lapListItem);
     lapsCounter++;
 }
+
 startBtn.addEventListener('click', startTimer);
 resetBtn.addEventListener('click', resetTimer);
 lapBtn.addEventListener('click', lapTimer);
